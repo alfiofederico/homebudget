d628332c5331b8b864e02de44f21b5db9e3d0446
Alfio Federico Di Pinto <fefferico81@gmail.com>
2024-01-09 10:04:58 +0900

    first commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,35 @@
+import { Form } from "react-router-dom"
+
+// library
+import { UserPlusIcon } from "@heroicons/react/24/solid";
+
+// assets
+import illustration from "../assets/illustration.jpg"
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div>
+        <h1>
+          <span className="accent">Your Money</span>
+        </h1>
+     
+        <Form method="post">
+          <input
+            type="text"
+            name="userName"
+            required
+            placeholder="Who are you?" aria-label="Your Name" autoComplete="given-name"
+          />
+          <input type="hidden" name="_action" value="newUser" />
+          <button type="submit" className="btn btn--dark">
+            <span>Create Account</span>
+            <UserPlusIcon width={20} />
+          </button>
+        </Form>
+      </div>
+      <img src={illustration} alt="Person with money" width={600} />
+    </div>
+  )
+}
+export default Intro
